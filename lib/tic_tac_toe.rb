9d30f5adb2be94d9e113eb50ec63ec6f979da0f6
Ben Parish <parishben13@gmail.com>
2020-05-27 05:52:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,13 +30,14 @@ def move(index, current_player)
   @board[index] = current_player
 end
 
-def position_taken?(index)
- @board[index] != " " || @board[index] != "" || !@board[index].nil?? true : false
-
+def position_taken?(location)
+  @board[location] != " " && @board[location] != ""
 end
+
 def valid_move?(index)
   index.between?(0,8) && !position_taken?(index)
-  end
+end
+
 
 def turn
   input = gets.strip
